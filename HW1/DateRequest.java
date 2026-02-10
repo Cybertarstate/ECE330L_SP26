1e423417e3b9acc4a1bf58c1a504b75aaaefb191
Jared G <124073725+Cybertarstate@users.noreply.github.com>
2026-02-10 18:54:53 +0000

    update 2

---
+++ HW1/DateRequest.java
@@ -0,0 +1,7 @@
+package HW1;
+
+public class DateRequest extends Request
+{
+    private static final long serialVersionUID = 321L;
+    
+}
---
+++ HW1/WorkRequest.java
@@ -0,0 +1,11 @@
+package HW1;
+
+public class WorkRequest extends Request
+{
+    private static final long serialVersionUID = 432L
+
+    public Object execute()
+    {
+        return null;
+    }
+}
